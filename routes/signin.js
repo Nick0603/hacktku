0cdda6539fb13ec09c19c1424289f01cbb09344f
Nick0603 <b10303008@gmail.com>
2018-04-01 14:00:13 +0800

    v1.0

--- routes/signin.js
+++ routes/signin.js
@@ -7,15 +7,15 @@ router.get('/', function (req, res) {
 });
 
 router.post('/', function (req, res) {
-    if (!req.body.name) {
-        //重複 或 未填入說鞥者名稱
-        res.redirect('/signin');
-    } else {
+    // if (!req.body.name) {
+    //     //重複 或 未填入說鞥者名稱
+    //     res.redirect('/signin');
+    // } else {
         let user_id = cryptoRandomString(5);
         res.cookie("user", req.body.name, { maxAge: 1000 * 60 * 60 * 24 * 30 });
         res.cookie("user_id", user_id, { maxAge: 1000 * 60 * 60 * 24 * 30 });
         res.redirect('/make_pair?user_id=' + user_id);
-    }
+    // }
 });
 
 module.exports = router;
